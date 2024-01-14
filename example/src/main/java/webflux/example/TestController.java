61247cdb3228a775e4dc72b04e15849a02fc171c
조정현[커머스ID개발] <jeong_hyun.jo@navercorp.com>
2024-01-14 21:47:07 +0900

    add sample code

---
+++ example/src/main/java/webflux/example/TestController.java
@@ -0,0 +1,26 @@
+package webflux.example;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Getter
+@NoArgsConstructor
+@RestController
+public class TestController {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getController(@PathVariable("id") String id) {
+        log.warn("result = {}", id);
+        return new ResponseEntity<>("ok", null, HttpStatusCode.valueOf(200));
+    }
+}
---
+++ example/src/test/java/webflux/example/config/TWebClientTest.java
@@ -0,0 +1,27 @@
+package webflux.example.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
+
+@SpringBootTest
+public class TWebClientTest {
+
+    @Autowired
+    private WebClient client;
+
+    @Test
+    void test() {
+        Mono<String> mono = client.get()
+            .uri("/persons/{id}", "11")
+            .accept(MediaType.APPLICATION_JSON)
+            .retrieve()
+            .bodyToMono(String.class)
+                .doOnSuccess()
+        System.out.println("mono.do = " + mono.do);
+    }
+}
