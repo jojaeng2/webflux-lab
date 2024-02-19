e4b5439c8551dca711ef935f85d59aaa6b2c3010
조정현[커머스ID개발] <jeong_hyun.jo@navercorp.com>
2024-02-19 23:59:05 +0900

    add sinks example01

---
+++ example/src/main/java/webflux/example/basic/sinks/SinksExample01.java
@@ -0,0 +1,43 @@
+package webflux.example.basic.sinks;
+
+import lombok.extern.slf4j.Slf4j;
+
+import reactor.core.publisher.Flux;
+
+@Slf4j
+public class SinksExample01 {
+    public static void main(String[] args) {
+        Flux.just("200", "401")
+            .map(num -> {
+                if (num == "200") {
+                    return num;
+                }
+                throw new RuntimeException(num);
+            })
+            .onErrorMap(throwable -> {
+                switch (throwable.getMessage()) {
+                    case "301":
+                        return new Exception("redirect");
+                    case "401":
+                        return new Exception("인증 에러");
+                    case "409":
+                        return new Exception("내부 conflict");
+                    case "500":
+                        return new Exception("내부 서버 에러");
+                }
+                return throwable;
+            })
+            /*
+                stream이 성공했을 경우에만 수행. exception은 처리 안함.
+             */
+            .handle((num, sinks) -> {
+                log.warn(num);
+                sinks.next(num);
+            })
+            .subscribe(
+                (number) -> log.info("number: {}", number),
+                (error) -> log.error("error: {}", error.getMessage()),
+                () -> log.warn("success message")
+            );
+    }
+}
